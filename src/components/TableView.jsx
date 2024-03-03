297d8775577c74f68a210f10d91c86fbfe9967f5
ELOUAIZI Ayoub <ayoubelouaizi@gmail.com>
2024-03-03 11:35:04 +0100

    adding some lyout for mynotes

--- src/components/TableView.jsx
+++ src/components/TableView.jsx
@@ -1,8 +1,14 @@
 "use client";
 import React, { useEffect, useState } from "react";
+import DeleteIcon from "./Icons/DeleteIcon";
+import UpdateIcon from "./Icons/UpdateIcon";
+import AddIcon from "./Icons/AddIcon";
+import NoteForm from "./NoteForm";
 
 const TableView = () => {
   const [notes, setNotes] = useState(null);
+  const [openForm, setOpenForm] = useState(false);
+
   //   setNotes([{}, {}, {}]);
   useEffect(() => {
     setNotes([
@@ -18,26 +24,32 @@ const TableView = () => {
   return (
     <div>
       <div class="p-5 h-screen bg-gray-100">
-        <h1 class="text-xl mb-2">Your orders</h1>
+        <div className="flex">
+          <h1 class="text-xl mb-2">Your notes</h1>
+          <div onClick={e=>setOpenForm(true)}>
+          <AddIcon />
+          </div>
+        </div>
+        <NoteForm openForm={openForm} setOpenForm={setOpenForm}/>
         {/* for large screen we use this */}
         <div class="overflow-auto rounded-lg shadow hidden md:block">
           <table class="w-full">
             <thead class="bg-gray-50 border-b-2 border-gray-200">
               <tr>
                 <th class="w-20 p-3 text-sm font-semibold tracking-wide text-left">
-                  No.
+                  Subject
                 </th>
                 <th class="p-3 text-sm font-semibold tracking-wide text-left">
                   Details
                 </th>
                 <th class="w-24 p-3 text-sm font-semibold tracking-wide text-left">
-                  Status
-                </th>
-                <th class="w-24 p-3 text-sm font-semibold tracking-wide text-left">
                   Date
                 </th>
-                <th class="w-32 p-3 text-sm font-semibold tracking-wide text-left">
-                  Total
+                <th class="w-16 p-3 text-sm font-semibold tracking-wide text-left">
+                  Edit
+                </th>
+                <th class="w-16 p-3 text-sm font-semibold tracking-wide text-left">
+                  Delete
                 </th>
               </tr>
             </thead>
@@ -46,22 +58,22 @@ const TableView = () => {
                 <tr class="bg-white" key={note.id}>
                   <td class="p-3 text-sm text-gray-700 whitespace-nowrap">
                     <a href="#" class="font-bold text-blue-500 hover:underline">
-                      10001
+                      Physics Homework
                     </a>
                   </td>
                   <td class="p-3 text-sm text-gray-700 whitespace-nowrap">
-                    Kring New Fit office chair, mesh + PU, black
-                  </td>
-                  <td class="p-3 text-sm text-gray-700 whitespace-nowrap">
-                    <span class="p-1.5 text-xs font-medium uppercase tracking-wider text-green-800 bg-green-200 rounded-lg bg-opacity-50">
-                      Delivered
-                    </span>
+                    Reminder to complete...
                   </td>
                   <td class="p-3 text-sm text-gray-700 whitespace-nowrap">
                     16/10/2021
                   </td>
-                  <td class="p-3 text-sm text-gray-700 whitespace-nowrap">
-                    $200.00
+                  <td class="p-3 text-sm  text-gray-700 whitespace-nowrap">
+                  <div onClick={e=>setOpenForm(true)}>
+                    <UpdateIcon />
+                    </div>
+                  </td>
+                  <td class="p-3 text-sm flex justify-center  text-gray-700 whitespace-nowrap">
+                    <DeleteIcon />
                   </td>
                 </tr>
               ))}
@@ -76,20 +88,20 @@ const TableView = () => {
                 <div class="flex items-center space-x-2 text-sm">
                   <div>
                     <a href="#" class="text-blue-500 font-bold hover:underline">
-                      #1000
+                      Physics Homework
                     </a>
                   </div>
                   <div class="text-gray-500">10/10/2021</div>
                   <div>
-                    <span class="p-1.5 text-xs font-medium uppercase tracking-wider text-green-800 bg-green-200 rounded-lg bg-opacity-50">
-                      Delivered
-                    </span>
+                    <UpdateIcon />
                   </div>
                 </div>
                 <div class="text-sm text-gray-700">
                   Kring New Fit office chair, mesh + PU, black
                 </div>
-                <div class="text-sm font-medium text-black">$200.00</div>
+                <div class="text-sm font-medium text-black">
+                  <DeleteIcon />
+                </div>
               </div>
             </div>
           ))}
